0e2116cbb779d80080d7cd753ba21a6da35a0521
Divya Devendra Singh <148423949+Divya-Devendrasingh@users.noreply.github.com>
2026-04-25 11:38:45 +0530

    Add function to find first non-repeating number
    Implement function to find first non-repeating number in a list.

--- problem_solving/non_repeating.py
+++ problem_solving/non_repeating.py
@@ -1,1 +1,12 @@
+def first_nonrep(numbers):
+    count = {}
 
+    for n in numbers:
+        count[n] = count.get(n, 0) + 1
+
+    for n in numbers:
+        if count[n] == 1:
+            return n
+
+
+print(first_nonrep([4, 5, 6, 5, 4, 3, 2]))
